9e480e12684bd33d31faa70e3bc17aa0cc2a05fa
Vsevolod <vmsk131uh@gmail.com>
2022-04-19 02:27:04 +0300

    added imageview to qrcode scanner with rect around detected qrcodes.

--- app/src/main/java/tk/kvakva/qrcodescanner002/MainActivity.kt
+++ app/src/main/java/tk/kvakva/qrcodescanner002/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.res.Configuration
 import android.graphics.*
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.params.StreamConfigurationMap
+import android.media.Image
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Build
@@ -63,6 +64,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cameraExecutor: ExecutorService
     private var cameraProvider: ProcessCameraProvider? = null
     private var imageAnalyzer: ImageAnalysis? = null
+
+    private val mYuvToRgbConverter by lazy { YuvToRgbConverter(this) }
 
     //    private lateinit var outputDirectory: File
     val scanner = BarcodeScanning.getClient(
@@ -634,6 +637,36 @@ class MainActivity : AppCompatActivity() {
                 if (barcodes.size > 0) {
                     delQrAnalyzer()
                     viewModelMaAc.qrScnOff()
+                    if (i.format == ImageFormat.YUV_420_888) {
+                        val b0 =Bitmap.createBitmap(i.width, i.height, Bitmap.Config.ARGB_8888)
+//                            if (i.imageInfo.rotationDegrees == 90 || i.imageInfo.rotationDegrees == 270) {
+//                                Bitmap.createBitmap(i.height, i.width, Bitmap.Config.ARGB_8888)
+//                            } else {
+//                                Bitmap.createBitmap(i.width, i.height, Bitmap.Config.ARGB_8888)
+//                            }
+                        mYuvToRgbConverter.yuvToRgb(i.image!!, b0)
+                        val b = if(i.imageInfo.rotationDegrees>0){
+                            val m = Matrix()
+                            m.postRotate(i.imageInfo.rotationDegrees.toFloat())
+                            Bitmap.createBitmap(b0,0,0,i.width,i.height,m,true)
+                        } else {
+                            b0
+                        }
+                        val c = Canvas(b)
+                        val p = Paint()
+                        p.color = Color.GREEN
+                        p.strokeWidth = 10f
+                        p.style = Paint.Style.STROKE
+                        p.alpha = 60
+                        for (barcode in barcodes) {
+                            if (barcode.boundingBox != null) {
+                                Log.e(TAG, "**** ${barcode.boundingBox}")
+                                c.drawRect(barcode.boundingBox!!, p)
+                            }
+                        }
+                        binding.picImageView.setImageBitmap(b)
+                        binding.picImageView.visibility = View.VISIBLE
+                    }
                 }
                 i.close()
                 // [END get_barcodes]
@@ -659,8 +692,16 @@ class MainActivity : AppCompatActivity() {
 
         imageAnalyzer = ImageAnalysis.Builder()
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .setTargetResolution(
+                if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+                    Size(viewModelMaAc.picSize.value.height, viewModelMaAc.picSize.value.width)
+                else
+                    viewModelMaAc.picSize.value
+                //Size(1080,1920)
+            )
             .build()
             .also {
+
                 it.setAnalyzer(cameraExecutor) { iprx ->
                     scanBarcodes(iprx)
                 }
@@ -680,6 +721,122 @@ class MainActivity : AppCompatActivity() {
             cameraProvider?.unbind(imageAnalyzer)
     }
 
+    private var pixelCount: Int = -1
+    private fun imageToByteArray(image: Image, outputBuffer: ByteArray) {
+        assert(image.format == ImageFormat.YUV_420_888)
+
+        val imageCrop = image.cropRect
+        val imagePlanes = image.planes
+
+        imagePlanes.forEachIndexed { planeIndex, plane ->
+            // How many values are read in input for each output value written
+            // Only the Y plane has a value for every pixel, U and V have half the resolution i.e.
+            //
+            // Y Plane            U Plane    V Plane
+            // ===============    =======    =======
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            val outputStride: Int
+
+            // The index in the output buffer the next value will be written at
+            // For Y it's zero, for U and V we start at the end of Y and interleave them i.e.
+            //
+            // First chunk        Second chunk
+            // ===============    ===============
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            var outputOffset: Int
+
+            when (planeIndex) {
+                0 -> {
+                    outputStride = 1
+                    outputOffset = 0
+                }
+                1 -> {
+                    outputStride = 2
+                    // For NV21 format, U is in odd-numbered indices
+                    outputOffset = pixelCount + 1
+                }
+                2 -> {
+                    outputStride = 2
+                    // For NV21 format, V is in even-numbered indices
+                    outputOffset = pixelCount
+                }
+                else -> {
+                    // Image contains more than 3 planes, something strange is going on
+                    return@forEachIndexed
+                }
+            }
+
+            val planeBuffer = plane.buffer
+            val rowStride = plane.rowStride
+            val pixelStride = plane.pixelStride
+
+            // We have to divide the width and height by two if it's not the Y plane
+            val planeCrop = if (planeIndex == 0) {
+                imageCrop
+            } else {
+                Rect(
+                    imageCrop.left / 2,
+                    imageCrop.top / 2,
+                    imageCrop.right / 2,
+                    imageCrop.bottom / 2
+                )
+            }
+
+            val planeWidth = planeCrop.width()
+            val planeHeight = planeCrop.height()
+
+            // Intermediate buffer used to store the bytes of each row
+            val rowBuffer = ByteArray(plane.rowStride)
+
+            // Size of each row in bytes
+            val rowLength = if (pixelStride == 1 && outputStride == 1) {
+                planeWidth
+            } else {
+                // Take into account that the stride may include data from pixels other than this
+                // particular plane and row, and that could be between pixels and not after every
+                // pixel:
+                //
+                // |---- Pixel stride ----|                    Row ends here --> |
+                // | Pixel 1 | Other Data | Pixel 2 | Other Data | ... | Pixel N |
+                //
+                // We need to get (N-1) * (pixel stride bytes) per row + 1 byte for the last pixel
+                (planeWidth - 1) * pixelStride + 1
+            }
+
+            for (row in 0 until planeHeight) {
+                // Move buffer position to the beginning of this row
+                planeBuffer.position(
+                    (row + planeCrop.top) * rowStride + planeCrop.left * pixelStride
+                )
+
+                if (pixelStride == 1 && outputStride == 1) {
+                    // When there is a single stride value for pixel and output, we can just copy
+                    // the entire row in a single step
+                    planeBuffer.get(outputBuffer, outputOffset, rowLength)
+                    outputOffset += rowLength
+                } else {
+                    // When either pixel or output have a stride > 1 we must copy pixel by pixel
+                    planeBuffer.get(rowBuffer, 0, rowLength)
+                    for (col in 0 until planeWidth) {
+                        outputBuffer[outputOffset] = rowBuffer[col * pixelStride]
+                        outputOffset += outputStride
+                    }
+                }
+            }
+        }
+    }
 
 }
 
